0db242dc6cd9dd96e0ed4968fa11136ba00e6bc5
Jarrod Parkes <parkesfjarrod@gmail.com>
2017-08-23 10:30:59 -0500

    upgrade web dev container and add perfect crypto

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 7),
-        .Package(url: "https://github.com/nicholasjackson/swift-mysql.git", majorVersion: 1, minor: 4)
+        .Package(url: "https://github.com/nicholasjackson/swift-mysql.git", majorVersion: 1, minor: 4),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", majorVersion: 1)
     ]
 )
 
