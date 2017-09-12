55dec5649c1843ff1bbd45f3b88695ac7feda476
Jarrod Parkes <parkesfjarrod@gmail.com>
2017-09-12 07:58:10 -0500

    start simple branch

--- Sources/UsersServer/main.swift
+++ Sources/UsersServer/main.swift
@@ -47,17 +47,11 @@ func cleanKeyString(_ string: String) -> String {
 }
 
 // FIXME: Use a service like S3 to deliver keys instead of injecting environment variables
-let privateKeyClean = cleanKeyString(env["PRIVATE_KEY"] ?? "")
-let publicKeyClean = cleanKeyString(env["PUBLIC_KEY"] ?? "")
-
-// Create JWT composer
-let jwtComposer = JWTComposer(
-    privateKey: privateKeyClean,
-    publicKey: publicKeyClean
-)
+let _ = cleanKeyString(env["PRIVATE_KEY"] ?? "")
+let _ = cleanKeyString(env["PUBLIC_KEY"] ?? "")
 
 // Create handlers
-let handlers = Handlers(dataAccessor: dataAccessor, accountKitClient: accountKitClient, jwtComposer: jwtComposer)
+let handlers = Handlers(dataAccessor: dataAccessor, accountKitClient: accountKitClient)
 
 // Create router and middleware
 let router = Router()
@@ -68,23 +62,13 @@ router.options("/*", handler: handlers.getOptions)
 
 // GET
 router.get("/*", middleware: CheckRequestMiddleware(method: .get))
-router.get("/*", middleware: JWTMiddleware(jwtComposer: jwtComposer, permissions: [.usersProfile, .usersAll]))
-router.get("/users/search", handler: handlers.searchUsers)
-router.get("/users/profile", handler: handlers.getCurrentUser)
-router.get("/users/:id", handler: handlers.getUsers)
-router.get("/users", handler: handlers.getUsers)
 
 // POST
 router.post("/*", middleware: CheckRequestMiddleware(method: .post))
 router.post("/users/login", handler: handlers.login)
-router.post("/users/logout", handler: handlers.logout)
 
 // PUT
 router.put("/*", middleware: CheckRequestMiddleware(method: .put))
-router.put("/users/profile", middleware: JWTMiddleware(jwtComposer: jwtComposer, permissions: [.usersProfile, .usersAll]))
-router.put("/users/profile", handler: handlers.updateProfile)
-router.put("/users/favorites", middleware: JWTMiddleware(jwtComposer: jwtComposer, permissions: [.usersAll]))
-router.put("/users/favorites", handler: handlers.updateFavorites)
 
 // Add an HTTP server and connect it to the router
 Kitura.addHTTPServer(onPort: 8080, with: router)
--- Sources/UsersService/Models/User/User+MySQLDataAccessor.swift
+++ Sources/UsersService/Models/User/User+MySQLDataAccessor.swift
@@ -28,127 +28,24 @@ public class UserMySQLDataAccessor: UserMySQLDataAccessorProtocol {
     // MARK: Queries
 
     public func getUsers(withIDs ids: [String], pageSize: Int = 10, pageNumber: Int = 1) throws -> [User]? {
-        let selectUsersIDs = MySQLQueryBuilder()
-            .select(fields: ["id"], table: "users")
-            .wheres(statement:"id IN (?)", parameters: ids)
-
-        // Select ids and apply pagination before joins
-        let simpleResults = try execute(builder: selectUsersIDs)
-        simpleResults.seek(offset: cacluateOffset(pageSize: pageSize, pageNumber: pageNumber))
-        let simpleUsers = simpleResults.toUsers(pageSize: pageSize)
-        let newIDs = simpleUsers.map({String($0.id!)!})
-
-        var users = [User]()
-
-        // Once the ids are determind, perform the joins
-        if newIDs.count > 0 {
-            let selectUser = MySQLQueryBuilder()
-                .select(fields: ["id", "name", "location", "photo_url", "created_at", "updated_at"], table: "users")
-            let selectFavorites = MySQLQueryBuilder()
-                .select(fields: ["user_id", "activity_id"], table: "favorites")
-            let selectQuery = selectUser.wheres(statement: "id IN (?)", parameters: newIDs)
-                .join(builder: selectFavorites, from: "id", to: "user_id", type: .LeftJoin)
-                .order(byExpression: "id", order: .Ascending)
-
-            let result = try execute(builder: selectQuery)
-            users = result.toUsers()
-        }
-
-        return (users.count == 0) ? nil : users
+        return nil
     }
 
     public func getUsers(pageSize: Int = 10, pageNumber: Int = 1) throws -> [User]? {
-        let selectUsersIDs = MySQLQueryBuilder()
-            .select(fields: ["id"], table: "users")
-
-        // Select ids and apply pagination before joins
-        let simpleResults = try execute(builder: selectUsersIDs)
-        simpleResults.seek(offset: cacluateOffset(pageSize: pageSize, pageNumber: pageNumber))
-        let simpleUsers = simpleResults.toUsers(pageSize: pageSize)
-        let ids = simpleUsers.map({String($0.id!)!})
-
-        var users = [User]()
-
-        // Once the ids are determind, perform the joins
-        if ids.count > 0 {
-            let selectUser = MySQLQueryBuilder()
-                .select(fields: ["id", "name", "location", "photo_url", "created_at", "updated_at"], table: "users")
-            let selectFavorites = MySQLQueryBuilder()
-                .select(fields: ["user_id", "activity_id"], table: "favorites")
-            let selectQuery = selectUser.wheres(statement: "id IN (?)", parameters: ids)
-                .join(builder: selectFavorites, from: "id", to: "user_id", type: .LeftJoin)
-                .order(byExpression: "id", order: .Ascending)
-
-            let result = try execute(builder: selectQuery)
-            users = result.toUsers()
-        }
-
-        return (users.count == 0) ? nil : users
+        return nil
     }
 
     // Upsert a stub user. If the user already exists, then nothing is updated and false is returned.
     public func upsertStubUser(_ user: User) throws -> Bool {
-        let upsertUser = MySQLQueryBuilder()
-                .upsert(data: user.toMySQLRow(), table: "users")
-
-        let result = try execute(builder: upsertUser)
-        return result.affectedRows > 0
+        return false
     }
 
     public func updateUser(_ user: User) throws -> Bool {
-        let updateQuery = MySQLQueryBuilder()
-                .update(data: user.toMySQLRow(), table: "users")
-                .wheres(statement: "Id=?", parameters: "\(user.id!)")
-
-        let result = try execute(builder: updateQuery)
-        return result.affectedRows > 0
+        return false
     }
 
     public func updateFavoritesForUser(_ user: User, favorites: [Int]) throws -> Bool {
-        let userID = "\(user.id!)"
-        let deleteFavoritesQuery = MySQLQueryBuilder()
-            .delete(fromTable: "favorites")
-            .wheres(statement: "user_id=?", parameters: userID)
-        var result: MySQLResultProtocol
-
-        guard let connection = try pool.getConnection() else {
-            Log.error("Could not get a connection")
-            return false
-        }
-        defer { pool.releaseConnection(connection) }
-
-        func rollbackEventTransaction(withConnection: MySQLConnectionProtocol, message: String) -> Bool {
-            Log.error("Could not update user's favorites: \(message)")
-            try! connection.rollbackTransaction()
-            return false
-        }
-
-        connection.startTransaction()
-
-        do {
-            // Delete existing favorites for user, so they can be replaced
-            result = try connection.execute(builder: deleteFavoritesQuery)
-            if result.affectedRows < 1 {
-                return rollbackEventTransaction(withConnection: connection, message: "Failed to delete existing event games during update")
-            }
-
-            // Insert favorites for user
-            for favorite in favorites {
-                let insertFavoriteQuery = MySQLQueryBuilder()
-                    .insert(data: ["user_id": userID, "activity_id": favorite], table: "favorites")
-                result = try connection.execute(builder: insertFavoriteQuery)
-                if result.affectedRows < 1 {
-                    return rollbackEventTransaction(withConnection: connection, message: "Failed to insert favorite with ID of \(favorite) into favorites")
-                }
-            }
-
-            try connection.commitTransaction()
-
-        } catch {
-            return rollbackEventTransaction(withConnection: connection, message: "updateFavoritesForUser failed")
-        }
-
-        return true
+        return false
     }
 
     // MARK: Utility
