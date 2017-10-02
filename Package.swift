65bb47901c2ea7263079f60c070256c16ab141dc
Jarrod Parkes <parkesfjarrod@gmail.com>
2017-10-02 13:39:31 -0500

    update mysql client

--- Package.swift
+++ Package.swift
@@ -13,8 +13,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 7),
-        //.Package(url: "https://github.com/nicholasjackson/swift-mysql.git", majorVersion: 1, minor: 7)
-        .Package(url: "https://github.com/jarrodparkes/swift-mysql", majorVersion: 1, minor: 7),
+        .Package(url: "https://github.com/nicholasjackson/swift-mysql.git", majorVersion: 1, minor: 8),        
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", majorVersion: 1)
     ]
 )
